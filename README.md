f5c0f6207d1eaf0128716c3b95fffc83cc5b9c0a
samwesleyptl <59816773+samwesleyptl@users.noreply.github.com>
2023-07-01 23:01:13 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,81 @@
-# data-science-portfolio
+# CBE Thermal Comfort Tool
+
+A web interface for comfort model calculations and visualizations according to ASHRAE Standard-55, EN Standard 16798 and ISO Standard 7730. 
+
+[Live deployment of the tool](http://comfort.cbe.berkeley.edu/).
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+## Prerequisites
+
+Python 3 installed on your machine.
+
+If you do not have Python installed on your machine you can follow [this guide](https://wiki.python.org/moin/BeginnersGuide/Download)
+
+## Installation
+
+This guide is for Mac OSX, Linux or Windows.
+
+**Check out from the repo.**
+```
+$ git clone https://github.com/CenterForTheBuiltEnvironment/comfort-tool.git
+$ cd comfort_tool
+```
+**Create a vitrual environment using the following command:**
+
+On Linux and MAC ` $ python3 -m venv venv `
+
+On Windows ` py -3 -m venv venv `
+
+**Activate the virtualenv:**
+
+On Linux and MAC ` $ . venv/bin/activate `
+
+On Windows ` venv\Scripts\activate `
+
+Your shell prompt will change to show the name of the activated environment.
+
+The dependencies of the comfort tool are all contained in *requirements.txt*. 
+Install them all using:
+`$ pip install -r requirements.txt`
+
+Now you should be ready to run the tool locally.
+`python3 comfort.py`
+
+Visit http://localhost:5000 in your browser to check it out. 
+Note that whenever you want to run the tool, you have to activate the virtualenv first.
+
+## Static files
+If you're serving static files from somewhere other than /static/, modify the static file paths ...
+
+Set STATIC_URL_PATH in `comfort.py`
+
+Set util.STATIC_URL_PATH in `static/js/util.js`
+
+Finally, manually change the path as needed in `static/html/leed.html` for the `/static/css/psychchart.css` stylesheet.
+
+## Features
+
+1. Models.
+    * Adaptive (inputs: air temperature, MRT, mean outdoor temperature, air speed)
+    * PMV with and without elevated air speed
+2. Clothing ensemble creator.
+3. Clothing and metabolic activity tables.
+4. Dual units (SI, IP).
+5. Local air speed control.
+6. Operative temperature specification or Air temperature and Mean Radiant Temperature specification.
+7. 1-to-1 humidity specification conversion (Relative humidity, vapor pressure, dew point, wet bulb, humidity ratio).
+8. Globe temperature to MRT converter – input: air temperature, air speed, globe temperature, globe diameter, globe emissivity. Output: Mean Radiant Temperature.
+9. Local thermal discomfort: short questionnaire that allows the user to assess risks of local thermal discomfort as defined by Standard 55.
+10. Interactive charts: psychrometric chart, temperature–relative humidity chart, adaptive chart.
+11. Mouse values: psychrometric variables displayed for each position of the cursor when the mouse moves on the chart.
+12. Dynamic predictive clothing calculator.
+13. Comfort Ranges: the ability to plot how the PMV and PPD ranges change in the psychrometric chart when clothing, metabolic activity, air speed, and mean radiant temperature variables are changed within a given range.
+14. Comparison of comfort conditions: possibility of visually comparing two or more thermal comfort simulations. Instead of manually transcribing the output of thermal comfort simulations with the tool, it would be practical to enable a simple visualization to compare several conditions.
+15. EN 15251 compliance: this section of the tool can be used to assess the compliance of indoor conditions to the European standard.
+
+
+## Documentation
+To edit or generate new documentation please refer to the README.md file which is located in the website folder.
---
+++ comfort.py
@@ -0,0 +1,197 @@
+from io import TextIOWrapper
+from flask import (
+    Flask,
+    request,
+    render_template,
+    send_from_directory,
+    abort,
+    jsonify,
+    make_response,
+)
+from pythermalcomfort.models import pmv_ppd, set_tmp, cooling_effect
+from pythermalcomfort.psychrometrics import v_relative, clo_dynamic
+import pandas as pd
+
+ALLOWED_EXTENSIONS = {"csv"}
+
+STATIC_URL_PATH = "/static"
+
+app = Flask(__name__, static_url_path=STATIC_URL_PATH)
+
+
+# define the extension files that can be uploaded by users
+def allowed_file(filename):
+    return "." in filename and filename.rsplit(".", 1)[1] in ALLOWED_EXTENSIONS
+
+
+@app.route("/download/<path:filename>")
+def download_file(filename):
+    if ".." in filename:
+        abort(404)
+    return send_from_directory(
+        "./media/", filename, mimetype="application/octet-stream"
+    )
+
+
+@app.route("/api/v1/comfort/pmv", methods=["GET"])
+def api_id():
+    # Check if an ID was provided as part of the URL.
+    # If ID is provided, assign it to a variable.
+    # If no ID is provided, display an error in the browser.
+    try:
+        clo = float(request.args["clo"])
+        ta = float(request.args["ta"])
+        tr = float(request.args["tr"])
+        v = float(request.args["v"])
+        met = float(request.args["met"])
+        rh = float(request.args["rh"])
+    except KeyError:
+        return "Error: You did not provided all the input parameters"
+
+    # Create an empty list for our results
+    results = dict()
+
+    value = pmv_ppd(ta, tr, v, rh, met, clo, wme=0, standard="ASHRAE")
+    results["data"] = []
+    results["data"].append(value)
+    results["message"] = "success"
+
+    # Use the jsonify function from Flask to convert our list of
+    # Python dictionaries to the JSON format.
+    return jsonify(results)
+
+
+# Upload page - The user can upload a csv with input environmental parameters and
+# returns a csv with indexes calculated
+@app.route(
+    "/upload"
+)  # tutorial https://stackoverflow.com/questions/27628053/uploading-and
+# -downloading-files-with-flask
+def upload():
+    return render_template("upload.html")
+
+
+# Upload page - The user can upload a csv with input environmental parameters and
+# returns a csv with indexes calculated
+@app.route(
+    "/other_tools"
+)  # tutorial https://stackoverflow.com/questions/27628053/uploading-and
+# -downloading-files-with-flask
+def other_tools():
+    return render_template("other_tools.html")
+
+
+# this function process the uploaded file and automatically downloads the file with the
+# results
+@app.route("/transform", methods=["POST"])
+def transform_view():
+    # get the uploaded file
+    request_file = request.files["data_file"]
+
+    # check file
+    if not request_file:
+        return "No file selected"
+    if not allowed_file(request_file.filename):
+        return "The file format is not allowed. Please upload a csv"
+
+    # read file
+    csv_file = TextIOWrapper(request_file, encoding="utf-8")
+    df = pd.read_csv(csv_file)
+    fields = {
+        "Air temperature": "ta",
+        "MRT": "tr",
+        "Air velocity": "vel",
+        "Relative humidity": "rh",
+        "Metabolic rate": "met",
+        "Clothing level": "clo",
+    }
+
+    si_unit = any(
+        [
+            True if ("Air temperature" in x) and (x.split(" [")[1] == "C]") else False
+            for x in df.columns
+        ]
+    )
+    df.columns = [fields[x.split(" [")[0]] if " [" in x else x for x in df.columns]
+
+    df["clo_dynamic"] = df.apply(
+        lambda row: clo_dynamic(clo=row["clo"], met=row["met"]), axis=1
+    )
+
+    results = []
+    ta = df["ta"].values
+    tr = df["tr"].values
+    vel = df["vel"].values
+    rh = df["rh"].values
+    met = df["met"].values
+    clo = df["clo"].values
+
+    for ix in range(df.shape[0]):
+        if si_unit:
+            units = "SI"
+            _vr = v_relative(vel[ix], met[ix])
+        else:
+            units = "IP"
+            _vr = v_relative(vel[ix] / 60 * 0.3048, met[ix]) * 3.28084
+
+        try:
+            _set = set_tmp(ta[ix], tr[ix], _vr, rh[ix], met[ix], clo[ix], units=units)
+        except:
+            _set = 9999
+        try:
+            _ce = cooling_effect(
+                ta[ix], tr[ix], _vr, rh[ix], met[ix], clo[ix], units=units
+            )
+        except:
+            _ce = 9999
+        try:
+            _pmv_ppd = pmv_ppd(
+                ta[ix],
+                tr[ix],
+                _vr,
+                rh[ix],
+                met[ix],
+                clo[ix],
+                standard="ashrae",
+                units=units,
+            )
+            _pmv = _pmv_ppd["pmv"]
+            _ppd = _pmv_ppd["ppd"]
+        except:
+            _pmv, _ppd = [9999, 9999]
+        results.append({"pmv": _pmv, "ppd": _ppd, "ce": _ce, "vr": _vr, "set": _set})
+
+    # split the pmv column in two since currently contains both pmv and ppd values
+    df_ = pd.DataFrame(results)
+    df = pd.concat([df, df_], axis=1, sort=False)
+    df["LEED compliance"] = [True if x < 10 else False for x in df.ppd]
+    df.loc[df.ppd == 9999, ["pmv", "ppd", "ce", "LEED compliance"]] = None
+
+    resp = make_response(df.to_csv(index=False))
+    resp.headers["Content-Disposition"] = "attachment; filename=export.csv"
+    resp.headers["Content-Type"] = "text/csv"
+    return resp
+
+
+@app.route("/compare")
+def compare():
+    return render_template("compare.html")
+
+
+@app.route("/ranges")
+def ranges():
+    return render_template("ranges.html")
+
+
+@app.route("/EN")
+def en():
+    return render_template("en.html")
+
+
+@app.route("/")
+def index():
+    return render_template("ashrae.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=False)
